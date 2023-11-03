c4ba0ac5c3d9fb42046314d08cdd8b70f17a6029
umbertofaenza <umberto.faenza@gmail.com>
2023-11-03 15:46:41 +0100

    axios call to API + console.log response array

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      $projects: [],
+    }
+  },
+
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        console.log(response.data)
+      })
+    }
+  },
+
+  created() {
+    this.fetchProjects();
+  }
+}
 </script>
 
 <template>
