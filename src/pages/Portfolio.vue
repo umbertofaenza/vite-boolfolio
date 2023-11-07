4ac0245ff9e9a0efac9465aab34bd15261087cf9
umbertofaenza <umberto.faenza@gmail.com>
2023-11-07 16:12:48 +0100

    create home, portfolio, project pages

---
+++ src/pages/Portfolio.vue
@@ -0,0 +1,27 @@
+<script>
+import ProjectCard from '../components/ProjectCard.vue'
+
+export default {
+    data() {
+        return{
+            pageTitle: 'Portfolio'
+        }
+    },
+
+    components: {
+        ProjectCard
+    }
+}
+</script>
+
+<template>
+    <h2 class="mb-3">
+        {{ pageTitle }}
+    </h2>
+    <ProjectCard/>
+    <!-- <div class="row g-3">
+        <ProjectCard v-for="project in this.projects" :project="project"/>
+    </div> -->
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/ProjectDetail.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return{
+            pageTitle: 'Project Detail'
+        }
+    },
+
+}
+</script>
+
+<template>
+    <h2 class="mb-3">
+        {{ pageTitle }}
+    </h2>
+</template>
+
+<style lang="scss" scoped></style>
