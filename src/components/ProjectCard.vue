186be802dd9784c21c04155596475705fae9af2a
umbertofaenza <umberto.faenza@gmail.com>
2023-11-07 15:40:20 +0100

    show project type and technologies in card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,7 +10,21 @@ export default {
     <div class="col-3">
         <div class="card h-100">
             <div class="card-header">
-                ID: {{ project.id }}
+
+                <div v-if="project.type">
+                    Type: {{ project.type.name }}
+                </div>
+                <div v-else>
+                    Type: No type.
+                </div>
+
+                <div v-if="project.technologies">
+                    Technologies: 
+                    <span v-for="technology in project.technologies" class="me-1">
+                        {{ technology.name }}
+                    </span>
+                </div>
+
             </div>
             <div class="card-body">
                 <h4 class="mb-3">{{ project.name }}</h4>
