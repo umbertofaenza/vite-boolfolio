31533d4531dc6fede1cbd0bdc26840df50660b55
umbertofaenza <umberto.faenza@gmail.com>
2023-11-07 16:11:26 +0100

    import router in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
+
 import './scss/style.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import {router} from './router/index.js'
+
+createApp(App).use(router).mount('#app')
