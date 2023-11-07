8a1bb1c1b3d5a225a553e31328445f4ec40ea048
umbertofaenza <umberto.faenza@gmail.com>
2023-11-07 16:58:34 +0100

    move axios call from App to Portfolio, print cards

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,11 @@
 <script>
-import axios from 'axios'
 
-import ProjectCard from './components/ProjectCard.vue'
 import Navbar from './ui/Navbar.vue'
 
 export default {
-  data() {
-    return {
-      projects: [],
-    }
-  },
 
-  components: { Navbar, ProjectCard },
+  components: { Navbar },
 
-  methods: {
-    fetchProjects() {
-      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-        this.projects = response.data;
-      })
-    }
-  },
-
-  created() {
-    this.fetchProjects();
-  }
 }
 </script>
 
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,16 +1,30 @@
 <script>
+import axios from 'axios'
 import ProjectCard from '../components/ProjectCard.vue'
 
 export default {
     data() {
         return{
-            pageTitle: 'Portfolio'
+            pageTitle: 'Portfolio',
+            projects: [],
         }
     },
 
     components: {
         ProjectCard
+    },
+
+    methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data;
+      })
     }
+  },
+
+  created() {
+    this.fetchProjects();
+  }
 }
 </script>
 
@@ -19,11 +33,12 @@ export default {
         <h2 class="mb-3">
             {{ pageTitle }}
         </h2>
+
+        <div class="row g-3">
+            <ProjectCard v-for="project in this.projects" :project="project"/>
+        </div>
     </div>
 
-    <!-- <div class="row g-3">
-        <ProjectCard v-for="project in this.projects" :project="project"/>
-    </div> -->
 </template>
 
 <style lang="scss" scoped></style>
