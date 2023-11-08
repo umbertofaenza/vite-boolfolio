fb592c61be6722edf559c78e2ed44679072d10ee
umbertofaenza <umberto.faenza@gmail.com>
2023-11-08 16:08:53 +0100

    add page for projects filtered by type

---
+++ src/pages/ProjectsByType.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return{
+            pageTitle: 'Projects by type',
+            projects: [],
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="mb-3">
+            {{ pageTitle }}
+        </h2>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
