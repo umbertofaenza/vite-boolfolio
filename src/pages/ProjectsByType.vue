042bd8fc1b66af0d0dc7c61018fbcc4fe5072d30
umbertofaenza <umberto.faenza@gmail.com>
2023-11-08 17:07:26 +0100

    import ProjectCard and print projects by type

--- src/pages/ProjectsByType.vue
+++ src/pages/ProjectsByType.vue
@@ -1,6 +1,8 @@
 <script>
 import axios from 'axios';
 
+import ProjectCard from '../components/ProjectCard.vue';
+
 export default {
     data() {
         return{
@@ -9,9 +11,15 @@ export default {
         }
     },
 
+    components: {
+        ProjectCard
+    },
+
     created() {
+
+
         axios.get("http://127.0.0.1:8000/api/projects-by-type/" + this.$route.params.type_id).then((response) => {
-        this.projects = response.data.data;
+        this.projects = response.data.projects;
       })
     }
 }
@@ -22,6 +30,10 @@ export default {
         <h2 class="mb-3">
             {{ pageTitle }}
         </h2>
+
+        <div class="row g-3">
+            <ProjectCard v-for="project in this.projects" :project="project" :isDetail="false"/>
+        </div>
     </div>
 </template>
 
